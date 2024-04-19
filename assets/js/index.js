84a5017e79b93a58252ac8dc448e803523b0a953
Aaron Jones <aaronjjj95@aol.com>
2024-04-19 21:34:45 +0000

    changed styles

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,7 +28,7 @@ function checker(input){
     switch(choices_object[input][computer_choice]){
         case 'win':
             result_ref.style.cssText = "background-color: darkgreen";
-                        result_ref.innerHTML = "YOU WIN";
+            result_ref.innerHTML = "YOU WIN";
             user_score++;
             break;
         case 'lose':
